9e8a9c6f4f3e410b19e98e28a9da976ed06f4ea0
Baltazarx <dandiajoesta@gmail.com>
2025-10-29 23:42:52 +0700

    baru

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/anniversary-1tahun/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
